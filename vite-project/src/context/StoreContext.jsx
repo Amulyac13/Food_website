1c5ba1b793bcd7dc58c77b77c9debe6dcb3d1943
Amulya Choudha <143872021+Amulyac13@users.noreply.github.com>
2024-07-15 02:05:56 +0530

    Update StoreContext.jsx

--- vite-project/src/context/StoreContext.jsx
+++ vite-project/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems]=useState({});
     const url=https://food-backend-lls3.onrender.com
-    // const url=import.meta.env.URL || "http://localhost:4000";
+    // const url=import.meta.env.URL || "http://localhost:4000"
 
     // to save the token we use a state variable.
     const [token,setToken]=useState("");
