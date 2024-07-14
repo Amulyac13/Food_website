8eb1ddc7e3368171f5db314c999857d1ac415ed9
Amulya Choudha <143872021+Amulyac13@users.noreply.github.com>
2024-07-14 22:19:42 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder=async(req,res)=>{
     // 5174
     console.log(req.body.data)
-    const frontend_url="http://localhost:5173"
+    const frontend_url="http://localhost:5173 || process.env.FRONTEND_URL"
     try {
         const newOrder=new orderModel({
             userId:req.body.userId,
